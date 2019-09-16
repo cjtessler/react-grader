5cabdba3f66d2954cf5ac956cf86e1efc4496b66
cjtessler <codytessler@gmail.com>
2019-09-15 20:57:29 -0400

    Average implemented

--- src/index.js
+++ src/index.js
@@ -3,49 +3,101 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function Results(props) {
+const averageGrade = (grades) => {
     // Calculate the average of the inputed grades
-    const sum = props.grades.reduce((a, b) => a + b);
+    const sum = grades.reduce((a, b) => a + b);
     var count = 0;
-    props.grades.forEach(function (element) {
+    grades.forEach(function (element) {
         if (element) {
             count++;
         }
     });
-    const avg = (count > 0) ? sum / count : "";
+    return ((count > 0) ? sum / count : "");
+}
 
-    // Calculate the score need to obtain a specifc grade
-    function handleSelect(event) {
-
+function GradeEntry(props) {
+    // Generate JSX for grade input
+    const items = [];
+    for (let i = 1; i <= 5; i++) {
+        items.push(
+            <label key={i}>
+                Test {i}: <span> </span>
+                <input type="text" onChange={props.onChange} name={i} />
+            </label>
+        );
     }
+    items.push(
+        <label key={6}>
+            Homework: <span> </span>
+            <input type="text" onChange={props.onChange} name="6" />
+        </label>
+    );
+
+    return (
+        <div className="form">
+            <form>
+                {items}
+            </form>
+        </div>
+    )
+}
+
+
+function Results(props) {
+
+    const avg = averageGrade(props.grades)
 
     return (
         <div>
             <p>Current Average: {Math.round(avg * 100) / 100} </p>
-            <p>Score needed on final for an
-                <select name="letter" onChange={handleSelect}>
-                    <option value="A">A</option>
-                    <option value="B">B</option>
-                    <option value="C">C</option>
-                    <option value="D">D</option>
-                </select>:
-            </p>
+
+            {/* Render if all grade are entered */}
+            {
+                props.grades.includes(null) &&
+                // <p>Score needed on final for an
+                // <select onChange={props.onChange}>
+                //         <option value="A">A</option>
+                //         <option value="B">B</option>
+                //         <option value="C">C</option>
+                //         <option value="D">D</option>
+                //     </select>:
+                // <span> {props.requiredGrade}</span>
+                // </p>
+
+                <table>
+                    <tr>
+                        <th>Desired Score</th>
+                        <th>Score on Final</th>
+                    </tr>
+                    <tr>
+                        <td>A</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>B</td>
+                        <td></td>
+                    </tr>
+                </table>
+            }
         </div>
     )
 }
+
 
 class GradeForm extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            grades: Array(6).fill(null)
+            grades: Array(6).fill(null),
+            requiredGrade: null
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSelectChange = this.handleSelectChange.bind(this);
     };
 
-    handleChange(event) {
+    handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
         const grades = this.state.grades.slice();
@@ -55,37 +107,33 @@ class GradeForm extends React.Component {
         this.setState({
             grades: grades
         });
+
+    }
+
+    handleSelectChange = (event) => {
+        const gradeMap = {
+            "A": 94,
+            "B": 84,
+            "C": 74,
+            "D": 64
+        };
+
+        // Pulls the desired letter grade from event
+        const value = event.target.value;
+
+        this.setState({
+            requiredGrade: gradeMap[value]
+        });
+
     }
 
     render() {
-        // Generate JSX for grade input
-        const items = [];
-        for (let i = 1; i <= 5; i++) {
-            items.push(
-                <label key={i}>
-                    Test {i}: <span> </span>
-                    <input type="text" onChange={this.handleChange} name={i} />
-                </label>
-            );
-        }
-        items.push(
-            <label key={6}>
-                Homework: <span> </span>
-                <input type="text" onChange={this.handleChange} name="6" />
-            </label>
-        );
-
-        // Return JSX
         return (
             <div className="page">
                 <h3>Explorations in Mathematics Grader</h3>
                 <p>Enter your grades in the form. </p>
-                <div className="form">
-                    <form onClick={this.handleSumbit}>
-                        {items}
-                    </form>
-                    <Results grades={this.state.grades} />
-                </div>
+                <GradeEntry onChange={this.handleChange} />
+                <Results onChange={this.handleSelectChange} grades={this.state.grades} requiredGrade={this.state.requiredGrade} />
             </div>
         );
     }
