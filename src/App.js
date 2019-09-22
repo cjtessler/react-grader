81059e94bfb7c402852ece854869df9c437d394c
cjtessler <codytessler@gmail.com>
2019-09-21 22:47:12 -0400

    updated js files

--- src/App.js
+++ src/App.js
@@ -1,58 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 import GradeEntry from "./components/GradeEntry";
+import Results from "./components/Results";
+
 import "./App.css";
 
-const averageGrade = grades => {
-  // Calculate the average of the inputed grades
-  const sum = grades.reduce((a, b) => a + b);
-  var count = 0;
-  grades.forEach(function(element) {
-    if (element) {
-      count++;
-    }
-  });
-  return count > 0 ? sum / count : "";
-};
-
-function Results(props) {
-  const avg = averageGrade(props.grades);
-  return (
-    <div>
-      <p>Current Average: {Math.round(avg * 100) / 100} </p>
-
-      {/* Render if all grade are entered */}
-      {/* {props.grades.includes(null) && (
-        <p>Score needed on final for an
-        <select onChange={props.onChange}>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
-            </select>:
-        <span> {props.requiredGrade}</span>
-        </p>
-
-        <table>
-          <tr>
-            <th>Desired Score</th>
-            <th>Score on Final</th>
-          </tr>
-          <tr>
-            <td>A</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>B</td>
-            <td></td>
-          </tr>
-        </table>
-      )} */}
-      <form className="form-group" onSubmit={props.onSubmit} >
-        <button className="btn btn-primary">Reset</button>
-      </form>
-    </div>
-  );
-}
 
 class App extends React.Component {
   constructor(props) {
@@ -93,7 +46,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="page">
+      <div className="container-fuild">
         <div className="page-header">
           <h3>Eplorations in Mathematics Grader</h3>
         </div>
@@ -120,5 +73,10 @@ class App extends React.Component {
     );
   }
 }
+
+App.propTypes = {
+  grades: PropTypes.arrayOf(PropTypes.number).isRequired,
+  isSubmitted: PropTypes.bool
+ }
 
 export default App;
--- src/components/GradeEntry.js
+++ src/components/GradeEntry.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React from "react";
 
 function GradeEntry(props) {
-    const items = [];
-    
-    // Create Test boxes
-    for (let i = 1; i <= 5; i++) {
-      items.push(
-        <label key={i}>
-          Test {i}: <span> </span>
-          <input type="text" onChange={props.onChange} name={i} autoComplete="off" className="form-control"/>
-        </label>
-      );
-    }
+  const items = [];
 
-    // Create Homework boxes
+  // Create Test boxes
+  for (let i = 1; i <= 5; i++) {
     items.push(
-      <label key={6}>
-        Homework: <span> </span>
-        <input type="text" onChange={props.onChange} name="6" autoComplete="off" className="form-control" />
+      <label key={i}>
+        Test {i}: <span> </span>
+        <input
+          type="number"
+          onChange={props.onChange}
+          name={i}
+          autoComplete="off"
+          className="form-control"
+          required
+          min="0"
+          max="100"
+        />
       </label>
-    );
-
-    // Return the form
-    return (
-      <div className="form-group col">
-        <label>Enter your grades in the form below.</label>
-        <form onSubmit={props.onSubmit}>
-          {items}
-          <button className="btn btn-primary">Submit</button>
-        </form>
-      </div>
     );
   }
 
-  export default GradeEntry;
+  // Create Homework boxes
+  // TODO: generalize this to create with test boxes
+  items.push(
+    <label key={6}>
+      Homework: <span> </span>
+      <input
+        type="number"
+        onChange={props.onChange}
+        name="6"
+        autoComplete="off"
+        className="form-control"
+        required
+        min="1"
+        max="100"
+      />
+    </label>
+  );
+
+  // Return the form
+  return (
+    <div className="form-group col">
+      <label>Enter your grades in the form below.</label>
+      <form onSubmit={props.onSubmit}>
+        {items}
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default GradeEntry;
