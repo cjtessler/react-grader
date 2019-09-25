2a63044f23f81886c9866a7b47d2072d00011a9d
cjtessler <codytessler@gmail.com>
2019-09-24 23:07:37 -0400

    Updates

---
+++ precache-manifest.bf41d19181f6ee4f737e6a0241a69230.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ff6b286ff2ee898f3cca9aa7355d36",
+    "url": "/react-grader/index.html"
+  },
+  {
+    "revision": "406f5a2b0895301a9447",
+    "url": "/react-grader/static/css/main.91c580b2.chunk.css"
+  },
+  {
+    "revision": "97ea7314884596660a6f",
+    "url": "/react-grader/static/js/2.66f997bd.chunk.js"
+  },
+  {
+    "revision": "406f5a2b0895301a9447",
+    "url": "/react-grader/static/js/main.936cb9a5.chunk.js"
+  },
+  {
+    "revision": "dc7d00fd3572bbf8c723",
+    "url": "/react-grader/static/js/runtime~main.30b0587a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-grader/precache-manifest.fc8342c4b113f516fb023153d95a176c.js"
+  "/react-grader/precache-manifest.bf41d19181f6ee4f737e6a0241a69230.js"
 );
 
 self.addEventListener('message', (event) => {
